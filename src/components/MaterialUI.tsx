15f9d1f2513b007b00762b9120ba511f7126c98a
加藤義隆 <991024yoshi@gmail.com>
2022-12-02 17:31:31 +0900

    material-ui

---
+++ src/components/MaterialUI.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import { Button, makeStyles, Typography, Grid, Paper } from "@material-ui/core"
+
+const useStyles = makeStyles({
+    btnStyle: {
+        backgroundColor: "green",
+        padding: "3px 50px",
+    },
+    typoStyle: {
+        color: "blue",
+    },
+    paperStyle: {
+        backgroundColor: "orange",
+        height: "50px",
+    }
+});
+
+const MaterialUI: React.FC = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Typography color="secondary" variant="h1" align="left" gutterBottom noWrap className={classes.typoStyle}>Hello UI</Typography>
+
+      <Grid container spacing={1}>
+          <Grid item xs={12}>
+              <Paper className={classes.paperStyle}>xs=12</Paper>
+          </Grid>
+          <Grid item xs={12} md={6}>
+              <Paper className={classes.paperStyle}>xs=6</Paper>
+          </Grid>
+          <Grid item xs={12} md={6}>
+              <Paper className={classes.paperStyle}>xs=6</Paper>
+          </Grid>
+          <Grid item xs={6} md={3}>
+              <Paper className={classes.paperStyle}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={6} md={3}>
+              <Paper className={classes.paperStyle}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={6} md={3}>
+              <Paper className={classes.paperStyle}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={6} md={3}>
+              <Paper className={classes.paperStyle}>xs=3</Paper>
+          </Grid>
+      </Grid>
+
+      <Button className={classes.btnStyle} variant="contained" color="primary">Test Button</Button>
+
+      <Grid container spacing={1} direction="column" justifyContent="center" alignItems="flex-end">
+          <Grid item xs={1}>
+              <Paper className={classes.paperStyle}>xs=1</Paper>
+          </Grid>
+          <Grid item xs={1}>
+              <Paper className={classes.paperStyle}>xs=1</Paper>
+          </Grid>
+          <Grid item xs={1}>
+              <Paper className={classes.paperStyle}>xs=1</Paper>
+          </Grid>
+      </Grid>
+      
+      <Grid container spacing={3} direction="column">
+          <Grid item xs={12} container>
+            <Grid item xs={2}>Demo</Grid>
+            <Grid item xs={8} />
+            <Grid item xs={2}>Test</Grid>
+          </Grid>
+          <Grid item xs={12} container justifyContent="space-around">
+              <Grid item xs={3}>
+                  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            　</Grid>
+              <Grid item xs={3}>
+                  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            　</Grid>
+              <Grid item xs={3}>
+                  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            　</Grid>
+          </Grid>
+      </Grid>
+
+    </div>
+  )
+}
+
+export default MaterialUI
